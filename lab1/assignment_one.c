2f0818086e359a988de7e2c03e6d5688af4b0959
matt5200 <leematt5@uw.edu>
2018-09-28 08:17:35 -0700

    made corrections

--- lab1/assignment_one.c
+++ lab1/assignment_one.c
@@ -1,40 +1,66 @@
-// preprocessor directive to support printing to the display
+
+/*
+Building Your Own Applications
+Now that you have successfully run an existing application on the target platform, this
+next exercise will have you write your own program and then make a series of changes to
+it to gain practice in some of the techniques that we will have to use in our more complex
+applications.
+
+Application 1
+Write a program that will display the letters: A B C D console and flash them
+together at approximately a one-second rate.
+*/
 
 #include <stdio.h>
-
-// the main program
-
-int main(void)
+int main()
 {
-	//  declare, efine, and inbitializa some local variables
-	int x =  9;
-	int y = 10
-	int z =  0;
-	float a = 0.0;
-
-	//  preform a simple calculation
-	z = x+y;
-	
-	//  print the results of the calculation to the display
-	printf ("the sum of x and y is %d\n" z,);
-	
-	//  ask the user for some data
-	printf("please enter a value\n");
-	
-	//  get the data from the user
-	//    the data will be a floating point number %f
-	//    stored in the variable 'a'
-	//    the & operator takes the address of the variable 'a'
-
-	scanf ("%f", &a);
-	
-	//  remove the newline from input buffer
-	getchar();
-	
-	//  print the user data to the display
-	//    the format will be xx.yy
-	
-	printf ("the data is %2.2f\n", a);
-
-	return 0;
+   // printf() displays the string inside quotation
+   printf("Hello, World!\n");
+   return 0;
 }
+
+/*
+Application 2
+Modify the program in Application 1 to print then erase the letters A then B then
+C then D at approximately a one-second rate.
+/*
+
+/*
+Application 3
+Modify the program in Application 1 to flash the letters A and C at a one-second
+rate and the letters B and D at a two-second rate.
+*/
+
+/*
+Application 4
+Modify the program to parameterize the two delay() function calls in the two for
+loops so that they will support different user specified delays rather than the
+single hard coded value as they are now. Where will those values have to be
+specified?
+*/
+
+/*
+Application 5
+Modify the program so that each of the two respective for loops is replaced by the
+following functions.
+void f1Data(unsigned long delay1);
+void f2Clear(unsigned long delay2);
+*/
+
+/*
+Application 6
+Modify the program so that parameter, delay, is passed into the two functions in
+part 4 above by reference rather than by value.
+*/
+
+/*
+Application 7
+Modify the program so that the two functions in part 5 above are replaced by a
+single function. The function should be able to be called with the character to be
+displayed and the value of the delay.
+
+/*
+Application 8
+Modify the program so that the function you wrote in part 6 is in a separate file.
+Your program will now be composed of two files.
+*/
