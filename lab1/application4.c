b8aebbd1c80920ca435d1b399add707f760a19b6
Matthew Lee <leematt5@uw.edu>
2018-09-30 17:55:35 -0700

    think its fixed

--- lab1/application4.c
+++ lab1/application4.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <windows.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 // the main program
 
 int main(void) {
@@ -17,60 +17,66 @@ int main(void) {
     int h;
     int i;
     int j;
+    int e = 0;
+    int f = 0;
     int count = 0;
+    // When t1 is true, then AB should be printed
+    bool t1 = 1;
+    // When t2 is true, the CD should be printed
+    // when both are true, then both should be printed
+    bool t2 = 1;
     // displays the correct sequence depending on the time elapsed
-    for (h = 0; h < 10; h++) {
-        count++;
-        if (count == 1) {
-            printf("abcd\r");
-            for (i = 0; i < one; i++) {   // delay
-                for (j = 0; j < two; j++) {
-                    {}
-                }
-            }
+    while (1==1) {
+
+    // Four possible prints
+    // A B
+    // C D
+    // ABCD
+
+    printf("ABCD\r");
+    // after the following code has executed, one t1 half period has passed
+    // and the A B letters must change state
+    for (long int c = 1; c <= one*100; c++) {
+        // if e has counted to two, then t2 must change state
+        e++;
+        if (e == two*100) {
+            e = 0;
+            f = 0;
+            t2 = !t2;
             system("cls");
-        } else if (count == 2) {
-            printf(" b d\r");
-            for (i = 0; i < one; i++) {   // delay
-                for (j = 0; j < two; j++) {
-                    {}
-                }
-            }
-            system("cls");
-        } else if (count == 3) {
-            printf("a c\r");
-            for (i = 0; i < one; i++) {    // delay
-                for (j = 0; j < two; j++) {
-                    {}
-                }
-            }
-            system("cls");
-        } else if (count == 4) {
-            printf(" b d\r");
-            for (i = 0; i < one; i++) {            // delay
-                for (j = 0; j < two; j++) {
-                    {}
-                }
-            }
-            system("cls");
-        } else if (count == 5) {
-            printf("abcd\r");
-            for (i = 0; i < one; i++) {
-                for (j = 0; j < two; j++) {
-                    {}
-                }
-            }
-            system("cls");
-        } else if (count == 6) {
-            printf("");
-            for (i = 0; i < one; i++) {           // delay
-                for (j = 0; j < two; j++) {
-                    {}
-                }
-            }
-            system("cls");
-            count = 0;
+        if (t1 == 1 && t2 ==1) {
+            // remove whatever is on screen and put on correct text
+            printf("ABCD\r");
         }
+        else if (t1 == 0 && t2 == 1) {
+            printf("CD\r");
+        }
+        else if (t1 == 1 && t2 == 0) {
+            printf("AB\r");
+        }
+        else {
+            system("cls");
+        }
+        }
+       for (long int d = 1; d <= one*100; d++) {
+        f++;
+       }}
+       t1 = !t1;
+       system("cls");
+       if (t1 == 1 && t2 ==1) {
+        // remove whatever is on screen and put on correct text
+        printf("ABCD\r");
+        }
+        else if (t1 == 0 && t2 == 1) {
+            printf("CD\r");
+        }
+        else if (t1 == 1 && t2 == 0) {
+            printf("AB\r");
+        }
+        else {
+            system("cls");
+        }
+
     }
 	return 0;
 }
