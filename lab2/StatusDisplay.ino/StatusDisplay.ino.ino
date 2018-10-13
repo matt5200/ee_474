8c74bfbe4ba6d9da376e2ba8ae790436f0fea090
Matthew T. Lee <leematt5@uw.edu>
2018-10-13 14:03:27 -0700

    added code

--- lab2/StatusDisplay.ino/StatusDisplay.ino.ino
+++ lab2/StatusDisplay.ino/StatusDisplay.ino.ino
@@ -1,1 +1,35 @@
 #include <TFT.h>
+
+typedef struct consoleDisplayData{ 
+  unsigned  short *nothing;
+  unsigned  short *batteryLevel;
+  unsigned short *fuelLevel;
+ // unsigned  short *batteryLevel;
+  unsigned short *powerConsumption;
+  unsigned short *powerGeneration;
+  bool *fuelLow;
+  bool *batteryLow;
+  bool *solarPanelState;
+} consoleDisplayData;
+
+typedef struct warningAlarmData{ 
+  unsigned short *nothing;
+  unsigned short *fuelLevel;
+  unsigned  short *batteryLevel;
+  bool *fuelLow;
+  bool *batteryLow;
+} warningAlarmData;
+
+
+
+typedef struct powerSubsystemData {
+  unsigned  short *nothing;
+  bool* solarPanelState;
+
+  unsigned short* batteryLevel;
+
+  unsigned short* powerConsumption;
+
+  unsigned short* powerGeneration;
+
+} powerSubsystemData;
