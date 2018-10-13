cd3c9b72e0a0977e250ec33214e9ad8ac8ce697d
kitriaa <kt78@uw.edu>
2018-10-13 12:43:44 -0700

    simplified code

--- lab2/powerSubsystem/satelliteComs.ino
+++ lab2/powerSubsystem/satelliteComs.ino
@@ -21,39 +21,23 @@ void satelliteComs(void* s) {
   *thrusterCommand = i;
   
   // convert decimal to binary
-    int binary[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int count = 15;
-    while (i != 0) {
-      int rem = i % 2;
-      if (rem == 1) {
-        i = i - 1;
-        binary[count] = 1;
-      }
-      count = count - 1;
-      i = i / 2;
+  int binary[16] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+  int count = 15;
+  while (i != 0) {
+    int rem = i % 2;
+    if (rem == 1) {
+      i = i - 1;
+      binary[count] = 1;
     }
+    count = count - 1;
+    i = i / 2;
+  }
 
   // interpret thrust command
-  if (binary[15] == 1) {
-    left = 1;
-  } else {
-    left = 0;
-  }
-  if (binary[14] == 1) {
-    right = 1;
-  } else {
-    right = 0;
-  }
-  if (binary[13] == 1) {
-    up = 1;
-  } else {
-    up = 0;
-  }
-  if (binary[12] == 1) {
-    down = 1;
-  } else {
-    down = 0;
-  }
+  left = binary[15];
+  right = binary[14];
+  up = binary[13];
+  down = binary[12];
   int magnitude[4] = {binary[8], binary[9], binary[10], binary[11]};
   int duration[8] = {binary[0], binary[1], binary[2], binary[3], binary[4], binary[5], binary[6], binary[7]};
 }
