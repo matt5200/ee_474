94a917d6f61c363a2be76511eeae8826e2221fe4
Matthew T. Lee <leematt5@uw.edu>
2018-10-21 10:35:28 -0700

    copied prv project code

---
+++ lab3/main/z_main.ino
@@ -0,0 +1,156 @@
+// Initialize all global variables
+bool solarPanelState;
+unsigned short batteryLevel;
+unsigned short powerConsumption;
+unsigned short powerGeneration;
+bool fuelLow;
+bool batteryLow;
+float fuelLevel;
+unsigned int thrusterCommand;
+
+// Initialize struct for TCB
+typedef struct TCB {
+void (*myTask)(void*);
+ void* taskDataPtr;
+} TCB; 
+
+// Initialize global variable for TCB array
+TCB fn[6];
+
+// Initialize all global structs
+powerSubsystemData psd;
+satelliteComsData sd;
+thrusterSubsystemData td;
+consoleDisplayData cd;
+warningAlarmData wd;
+
+  
+void setup() {
+
+  // Begin communication between board and computer
+  Serial.begin(9600);
+  // Set booleans for battery and fuel flash to true
+  batt_flash = true;
+  fuel_flash = true;
+  fuel_flash_two = true;
+
+  // The following code is for the TFT display
+  #ifdef USE_Elegoo_SHIELD_PINOUT
+  Serial.println(F("Using Elegoo 2.4\" TFT Arduino Shield Pinout"));
+  #else
+  Serial.println(F("Using Elegoo 2.4\" TFT Breakout Board Pinout"));
+  #endif
+
+  Serial.print("TFT size is "); Serial.print(tft.width()); Serial.print("x"); Serial.println(tft.height());
+
+  tft.reset();
+
+   uint16_t identifier = tft.readID();
+   if(identifier == 0x9325) {
+    Serial.println(F("Found ILI9325 LCD driver"));
+  } else if(identifier == 0x9328) {
+    Serial.println(F("Found ILI9328 LCD driver"));
+  } else if(identifier == 0x4535) {
+    Serial.println(F("Found LGDP4535 LCD driver"));
+  }else if(identifier == 0x7575) {
+    Serial.println(F("Found HX8347G LCD driver"));
+  } else if(identifier == 0x9341) {
+    Serial.println(F("Found ILI9341 LCD driver"));
+  } else if(identifier == 0x8357) {
+    Serial.println(F("Found HX8357D LCD driver"));
+  } else if(identifier==0x0101)
+  {     
+      identifier=0x9341;
+       Serial.println(F("Found 0x9341 LCD driver"));
+  }
+  else if(identifier==0x1111)
+  {     
+      identifier=0x9328;
+       Serial.println(F("Found 0x9328 LCD driver"));
+  }
+  else {
+    Serial.print(F("Unknown LCD driver chip: "));
+    Serial.println(identifier, HEX);
+    Serial.println(F("If using the Elegoo 2.8\" TFT Arduino shield, the line:"));
+    Serial.println(F("  #define USE_Elegoo_SHIELD_PINOUT"));
+    Serial.println(F("should appear in the library header (Elegoo_TFT.h)."));
+    Serial.println(F("If using the breakout board, it should NOT be #defined!"));
+    Serial.println(F("Also if using the breakout, double-check that all wiring"));
+    Serial.println(F("matches the tutorial."));
+    identifier=0x9328;
+  
+  }
+  tft.begin(identifier);
+  tft.fillScreen(BLACK);
+
+  // Set all of the global variables and structs
+  solarPanelState = false;
+  batteryLevel = 15;
+  powerConsumption = 0;
+  powerGeneration = 0;
+  cycle = 1;
+  reverse = 0;
+  fuelLow = false;
+  batteryLow = false;
+  fuelLevel = 7;
+  thrusterCommand = 0;
+  // Set the struct pointers for the power subsystem
+  psd.solarPanelState = &solarPanelState;
+  psd.batteryLevel = &batteryLevel;
+  psd.powerConsumption = &powerConsumption;
+  psd.powerGeneration = &powerGeneration;
+  // Set the struct pointers for the satellite comms
+  sd.fuelLow = &fuelLow;
+  sd.batteryLow = &batteryLow;
+  sd.solarPanelState = &solarPanelState;
+  sd.batteryLevel = &batteryLevel;
+  sd.fuelLevel = &fuelLevel;
+  sd.powerConsumption = &powerConsumption;
+  sd.powerGeneration = &powerGeneration;
+  sd.thrusterCommand = &thrusterCommand;
+  // Set the struct pointers for the thruster system
+  td.fuelLevel = &fuelLevel;
+  td.thrusterCommand = &thrusterCommand;
+  // Set the struct pointers for the warning data
+  wd.batteryLow = &batteryLow; 
+  wd.fuelLevel = &fuelLevel;
+  wd.fuelLow = &batteryLow; 
+  wd.batteryLevel = &batteryLevel;
+  // Set the struct pointers for the console display data.
+  cd.fuelLow = &fuelLow;
+  cd.batteryLow = &batteryLow;
+  cd.solarPanelState = &solarPanelState;
+  cd.fuelLevel = &fuelLevel;
+  cd.batteryLevel = &batteryLevel;
+  cd.powerConsumption = &powerConsumption;
+  cd.powerGeneration = &powerGeneration;
+  randomSeed(analogRead(0));
+  // Set the task queue array' pointers to the correct functions
+  // and data for each task
+   fn[0].myTask = &powerSubsystem;
+   fn[0].taskDataPtr = &psd;
+   fn[1].myTask = &thrusterSubsystem;
+   fn[1].taskDataPtr = &td;
+   fn[2].myTask = &satelliteComs;
+   fn[2].taskDataPtr = &sd;
+   fn[3].myTask = &ConsoleDisplay;
+   fn[3].taskDataPtr = &cd;
+   fn[4].myTask = &WarningAlarm;
+   fn[4].taskDataPtr = &wd;
+}
+
+void loop() {
+  
+
+  delay(1000);
+  // Print statement to veriy board is updating
+  Serial.println("****");
+
+  // Delay between cycles is for minor cycle requirements
+   for (int i = 0; i < 4; i++) {
+      (fn[i].myTask)(fn[i].taskDataPtr);
+       delay(1000);
+      (fn[4].myTask)(fn[4].taskDataPtr);
+  }
+  cycle++;
+}
