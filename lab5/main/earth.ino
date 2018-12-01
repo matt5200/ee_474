b65d6063f269fa32160a3b608dec0d9914f37def
matt5200 <leematt5@uw.edu>
2018-12-01 13:07:38 -0800

    updated earth

--- lab5/main/earth.ino
+++ lab5/main/earth.ino
@@ -1,7 +1,7 @@
 
 
 // Struct containing all data relevant to warning alarm system
-typedef struct void earthData { 
+typedef struct earthData { 
   unsigned  short *batteryLevel;
   float *fuelLevel;
   unsigned short *powerConsumption;
@@ -10,19 +10,36 @@ typedef struct void earthData {
   bool *solarPanelState;
   float *transportDist;
   unsigned int* imageData;
-} remoteData;
+  char earthCommand;
+  char satResponse;
+} earthData;
 
 void earth(void* r) {
-
-  remoteData* rdc = (warningAlarmData*) d;
-  Serial.print(rdc->fuelLow);
+  earthData*  = (earthData*) d;
+  Serial.println("What is earth command??");
+  while(Serial.availible() > 0) {
+     Serial.read();
+  }
+  goodDelay();
+  command = Serial.read();
+  Serial.print("Fuel Low: ");
+  Serial.println(rdc->fuelLow);
+  Serial.print("Battery Low :");
   Serial.print(rdc->batteryLow);
+  Serial.print("Solar Panel State :");
   Serial.print(rdc->solarPanelState);
+  Serial.print();
   Serial.print(rdc->fuelLevel);
+  Serial.print();
   Serial.print(rdc->powerConsumption);
+  Serial.print();
   Serial.print(rdc->powerGeneration);
+  Serial.print();
   Serial.print(rdc->batteryLevel);
+  Serial.print();
   Serial.print(rdc->batteryTemp);
+  Serial.print();
   Serial.print(rdc->transportDistance);
+  Serial.print();
   Serial.print(rdc->imageData);
 }
